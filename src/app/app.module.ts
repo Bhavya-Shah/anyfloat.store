78aa2caa157b261d726b347e848338f471cd6003
Bhavya-Shah <2679.stkabirnav@gmail.com>
2020-05-01 01:01:58 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { CatalogComponent } from './catalog-page/components/catalog/catalog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CatalogComponent,
   ],
   imports: [
     BrowserModule,
