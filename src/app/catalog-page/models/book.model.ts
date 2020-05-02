c611f8cafb2b308b6acd89e077044cbfc4f1f232
Bhavya-Shah <2679.stkabirnav@gmail.com>
2020-05-02 07:10:54 +0530

    book model

---
+++ src/app/catalog-page/models/book.model.ts
@@ -0,0 +1,11 @@
+export class Book{
+    public id: number
+    public name: string
+    public image: string
+    public year_of_release: number
+    public price: number
+    public quantity: number
+    public writer: string
+    public preview: string
+    public pages: number
+}
