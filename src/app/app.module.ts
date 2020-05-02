ef993b1bab78da3ceedc210fb636b3fa141ccb66
Bhavya-Shah <2679.stkabirnav@gmail.com>
2020-05-02 07:10:29 +0530

    book-list andbook-item components added in app modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { CatalogComponent } from './catalog-page/components/catalog/catalog.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BookListComponent } from './catalog-page/components/book-list/book-list.component';
+import { BookItemComponent } from './catalog-page/components/book-list/book-item/book-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     CatalogComponent,
+    BookListComponent,
+    BookItemComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
