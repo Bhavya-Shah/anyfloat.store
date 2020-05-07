b65f08b3192bec3486944df55ae87b1536e73fef
Bhavya-Shah <2679.stkabirnav@gmail.com>
2020-05-08 00:14:06 +0530

    added cart routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {path: '', loadChildren: ()=> import('./catalog-page/catalog-page.module').then(m=>m.CatalogPageModule)},
   {path: 'details', loadChildren: ()=> import('./details-page/details-page.module').then(m=>m.DetailsPageModule)},
-  {path: 'wishlist', loadChildren:()=> import('./wishlist-page/wishlist-page.module').then(m=>m.WishlistPageModule)}
+  {path: 'wishlist', loadChildren:()=> import('./wishlist-page/wishlist-page.module').then(m=>m.WishlistPageModule)},
+  {path: 'cart', loadChildren: ()=> import('./cart-page/cart-page.module').then(m=>m.CartPageModule)}
 ];
 
 @NgModule({
