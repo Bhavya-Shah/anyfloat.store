557825683fc2669c2f1f723137dcb283060bc3dd
Bhavya-Shah <2679.stkabirnav@gmail.com>
2020-05-14 09:41:34 +0530

    removed ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SharedModule } from './shared/shared.module';
-
 import { AppComponent } from './app.component';
 import { AuthGuardService } from './checkout-page/services/auth-guard.service';
 
@@ -17,7 +15,6 @@ import { AuthGuardService } from './checkout-page/services/auth-guard.service';
     SharedModule,
     BrowserModule,
     AppRoutingModule,
-    // NgbModule,
     HttpClientModule
   ],
   providers: [AuthGuardService],
